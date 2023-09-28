0c95340871bde350637a27c834e412894bfae144
Berzan <berzan@tutanota.com>
2023-09-28 18:18:40 +0000

    update: main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import { contract } from '$lib/stores/contract';
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	//onMount(contract.fetch);
 </script>
 
 <slot />
