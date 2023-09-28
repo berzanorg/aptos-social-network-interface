7c0836fd0b977af7aafcd18586216f71f719a2d6
Berzan <berzan@tutanota.com>
2023-09-28 17:54:38 +0000

    create: contract store

---
+++ src/lib/stores/contract.ts
@@ -0,0 +1,53 @@
+import { MODULE_ADDRESS } from '$lib/utils/contract';
+import { Network, Provider } from 'aptos';
+import { writable } from 'svelte/store';
+
+interface SocialNetworkData {
+	users: Array<User>;
+}
+
+interface User {
+	addr: string;
+	name: string;
+	bio: string;
+	pfp: string;
+	posts: Array<Post>;
+}
+
+interface Post {
+	content: string;
+	image: string;
+	comments: Array<Comment>;
+	like_count: number;
+}
+
+interface Comment {
+	addr: string;
+	content: string;
+	like_count: number;
+}
+
+const createContractStore = () => {
+	const { set, subscribe } = writable<SocialNetworkData>({
+		users: []
+	});
+
+	return {
+		subscribe,
+		fetch: async () => {
+			const provider = new Provider(Network.DEVNET);
+
+			const res = await provider.getAccountResource(
+				MODULE_ADDRESS,
+				`${MODULE_ADDRESS}::social_network::SocialNetwork`
+			);
+			const { users } = res.data as SocialNetworkData;
+
+			set({
+				users
+			});
+		}
+	};
+};
+
+export const contract = createContractStore();
