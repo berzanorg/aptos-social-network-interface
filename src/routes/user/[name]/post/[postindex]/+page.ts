1508674709c38a4d94141081671824532c87d1a0
Berzan <berzan@tutanota.com>
2023-09-28 18:21:20 +0000

    add: types

--- src/routes/user/[name]/post/[postindex]/+page.ts
+++ src/routes/user/[name]/post/[postindex]/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ params }) => {
 	return {
-		name: params.name
+		name: params.name,
+		postindex: params.postindex
 	};
 };
