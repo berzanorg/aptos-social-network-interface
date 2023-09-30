4b7b3a352f976924fcce2439915e1e7d5f908c50
Berzan <berzan@tutanota.com>
2023-09-30 17:31:15 +0000

    create: `CardCreateProfile` component

---
+++ src/lib/components/CardCreateProfile.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<div class="flex items-center px-6 py-7 justify-between border-b border-neutral-800">
+	<p class="text-lg text-neutral-700">You don't have a profile yet.</p>
+	<a
+		class="flex items-center px-5 h-9 text-lg font-semibold rounded-full border border-neutral-800 hover:border-neutral-600 hover:bg-neutral-900 hover:scale-95 duration-150"
+		href="{base}/profile">Create a Profile</a
+	>
+</div>
