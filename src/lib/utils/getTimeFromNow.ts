945575e2005f1f84b159eafd073d6d5c4caf1a0e
Berzan <berzan@tutanota.com>
2023-09-30 17:37:23 +0000

    define: `getTimeFromNow` function

---
+++ src/lib/utils/getTimeFromNow.ts
@@ -0,0 +1,19 @@
+export function getTimeFromNow(timestampAsSec: number) {
+	// Create Date objects for the two dates
+	const past = timestampAsSec * 1000;
+	const now = Date.now();
+
+	// Calculate the time difference in milliseconds
+	var timeDiff = Math.abs(now - past);
+
+	// Convert the time difference to days
+	const mins = Math.floor(timeDiff / (1000 * 60));
+
+	if (mins === 0) {
+		return 'now';
+	} else if (mins < 60) {
+		return `${mins} mins ago`;
+	} else {
+		return `${mins / 60} hours ago`;
+	}
+}
