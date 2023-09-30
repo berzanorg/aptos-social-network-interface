f2808ecd750d8c41623cdc63f0659567d3cc2e9c
Berzan <berzan@tutanota.com>
2023-09-30 17:32:44 +0000

    create: `CardInformConnection` component

---
+++ src/lib/components/CardInformConnection.svelte
@@ -0,0 +1,10 @@
+<p class="flex px-6 py-7 gap-2 border-b border-neutral-800 text-lg text-neutral-700">
+	<a
+		class="font-semibold underline hover:text-neutral-500 duration-150"
+		href="https://petra.app/"
+		target="_blank"
+	>
+		Petra
+	</a>
+	wallet is not connected.
+</p>
