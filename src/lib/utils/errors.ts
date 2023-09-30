a03b9607ecc544ba4eb4382c8adc52005f7804bf
Berzan <berzan@tutanota.com>
2023-09-30 17:37:05 +0000

    add: new error variants

--- src/lib/utils/errors.ts
+++ src/lib/utils/errors.ts
@@ -1,3 +1,11 @@
 export enum Errors {
-	AnyWalletNotFound = 'Any wallet is not found.'
+	AnyWalletNotFound = 'Any wallet is not found.',
+	OnlyUrlPastingIsAllowed = 'Only pasting a URL is allowed.',
+	NoProfilePictureURL = 'Profile picture URL is empty.',
+	NoName = 'Name is empty.',
+	NoBio = 'Biography is empty.',
+	NoPostContent = 'Post has no content.',
+	PostNotFound = 'Post not found',
+	ProfileNotEdited = 'Profile is not edited.',
+	NoCommentContent = 'Post has no content.'
 }
