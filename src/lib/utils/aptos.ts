556a385578b101c059dd14f20675c757a5988d8a
Berzan <berzan@tutanota.com>
2023-09-30 17:36:53 +0000

    rename: `contract.ts` to `aptos.ts`

---
+++ src/lib/utils/aptos.ts
@@ -0,0 +1,5 @@
+import { Network, Provider } from 'aptos';
+
+export const MODULE_ADDRESS = '0x8defbce28c294ef554e6d61297885e26b9cbdbb506aea6d70e16decd98cc1d1c';
+
+export const provider = new Provider(Network.DEVNET);
